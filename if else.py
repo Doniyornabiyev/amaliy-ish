d38d87501e3350532c7a3b3c000286fc80a51c89
Doniyornabiyev <155424276+Doniyornabiyev@users.noreply.github.com>
2024-01-08 11:28:35 +0500

    login
    foydalanuvchidan login suraydi
    agar u admin bulsa SAlom admin
    ruyxatni kuraszmi deydi.

---
+++ if else.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  8 11:01:23 2024
+
+@author: ASUS
+"""
+
+login = input("loginni kiriting")
+if login == "admin":
+    print("xush kelibsiz admin, ruyxatni kurasizmi")
+else:
+      print(f"xush kelibsiz {login}")
+   
